3bdfdfdab18e6475792a6ae043411c36e2d99951
kriszos <krzysztof.szostak@gmx.com>
2023-07-09 11:49:35 +0200

    Update alp-build-chr.sh

--- alp-build-chr.sh
+++ alp-build-chr.sh
@@ -3,6 +3,8 @@ echo "install packages"
 apk add qemu-img curl rsync gptfdisk dosfstools efibootmgr lsblk
 #modprobe nbd max_part=8
 modprobe vfat
+modprobe ext2
+modprobe ext3
 echo "download"
 #wget --no-check-certificate https://download.mikrotik.com/routeros/7.11beta4/chr-7.11beta4.img.zip -O /run/chr.img.zip
 wget --no-check-certificate https://download.mikrotik.com/routeros/7.8/chr-7.8.img.zip -O /run/chr.img.zip

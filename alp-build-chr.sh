00b09846caa9b5f779ffadf40b67f47a035de5a8
kriszos <krzysztof.szostak@gmx.com>
2023-07-09 11:05:15 +0200

    Update alp-build-chr.sh

--- alp-build-chr.sh
+++ alp-build-chr.sh
@@ -1,46 +1,48 @@
 #!/bin/sh
 echo "install packages"
 apk add qemu-img curl rsync gptfdisk dosfstools efibootmgr lsblk
-modprobe nbd max_part=8
+#modprobe nbd max_part=8
 modprobe vfat
 echo "download"
 #wget --no-check-certificate https://download.mikrotik.com/routeros/7.11beta4/chr-7.11beta4.img.zip -O /run/chr.img.zip
 wget --no-check-certificate https://download.mikrotik.com/routeros/7.8/chr-7.8.img.zip -O /run/chr.img.zip
 echo "unzip"
 unzip -p /run/chr.img.zip > /run/chr.img
-echo "convert raw to qcow2"
-qemu-img convert -f raw -O qcow2 /run/chr.img /root/chr.qcow2
-#qemu-img convert -f vhdx -O qcow2 /run/chr.img chr.qcow2
-echo "connect image as /dev/nbd0"
-qemu-nbd -c /dev/nbd0 /root/chr.qcow2
-partprobe /dev/nbd0
+#echo "convert raw to qcow2"
+#qemu-img convert -f raw -O qcow2 /run/chr.img /root/chr.qcow2
+#echo "connect image as /dev/nbd0"
+#qemu-nbd -c /dev/nbd0 /root/chr.qcow2
+echo "connect image as /dev/loop5"
+losetup -P /dev/loop5 /run/chr.img
+#partprobe /dev/nbd0
+partprobe /dev/loop5
 echo "create tmp directories"
-mkdir /run/tmpmount/
-mkdir /run/tmpefipart/
+mkdir /run/tmpmount
+mkdir /run/tmpefipart
 echo "mount first partition"
-mount -t ext2 /dev/nbd0p1 /run/tmpmount/
+mount -t ext2 /dev/loop5p1 /run/tmpmount/
 echo "copy efi/boot files from first partition"
 rsync -a /run/tmpmount/ /run/tmpefipart/
 echo "umount first partition"
 #umount /dev/nbd0p1
 umount /run/tmpmount
-partprobe /dev/nbd0
+partprobe /dev/loop5
 echo "format first partion as fat32"
 #mkfs.fat /dev/nbd0p1
-mkfs.vfat /dev/nbd0p1
-partprobe /dev/nbd0
+mkfs.vfat /dev/loop5p1
+partprobe /dev/loop5
 echo "mount first partition"
-mount -t vfat /dev/nbd0p1 /run/tmpmount/
+mount -t vfat /dev/loop5p1 /run/tmpmount/
 echo "copy efi/boot files to first partition"
 rsync -a /run/tmpefipart/ /run/tmpmount/
 echo "umount first partition"
-partprobe /dev/nbd0
+partprobe /dev/loop5
 #umount /dev/nbd0p1
 umount /run/tmpmount
 echo "mount second partition"
-partprobe /dev/nbd0
-mount -t ext3 /dev/nbd0p2 /run/tmpmount/
-partprobe /dev/nbd0
+partprobe /dev/loop5
+mount -t ext3 /dev/loop5p2 /run/tmpmount/
+partprobe /dev/loop5
 #echo
 #echo "in 5 seconds you can modify initial config of chr"
 #sleep 5
@@ -50,10 +52,10 @@ echo "curl from lxd user-data"
 curl -s --unix-socket /dev/lxd/sock lxd/1.0/config/cloud-init.user-data >> /run/tmpmount/rw/autorun.scr
 #nano /run/tmpmount/rw/autorun.scr
 echo "umount second partition"
-partprobe /dev/nbd0
+partprobe /dev/loop5
 #umount /dev/nbd0p2
 umount /run/tmpmount
-partprobe /dev/nbd0
+partprobe /dev/loop5
 echo "modify partition table"
 #exit 0
 (
@@ -72,16 +74,17 @@ echo n # Set the bootable flag? (Y/N)
 echo n # Unused partition space(s) found. Use one to protect more partitions? (Y/N)
 echo w # write changes to disk
 echo y # confirm
-) | gdisk /dev/nbd0
-partprobe /dev/nbd0
+) | gdisk /dev/loop5
+partprobe /dev/loop5
 sync
-echo "disconnect image from /dev/nbd0"
-qemu-nbd -d /dev/nbd0
-partprobe /dev/nbd0
+echo "disconnect image from /dev/loop5"
+#qemu-nbd -d /dev/loop5
+losetup -d /dev/loop5
+partprobe /dev/loop5
 echo "script finished, created file chr.qcow2"
 #qemu-img convert -f qcow2 -O vhdx chr.qcow2 chr.vhdx
-echo "convert to img"
-qemu-img convert -f qcow2 -O raw /root/chr.qcow2 /root/chr.img
+#echo "convert to img"
+#qemu-img convert -f qcow2 -O raw /root/chr.qcow2 /root/chr.img
 #exit 0
 #echo "wait 180 seconds to avoid race condition in cloud-init"
 #sleep 180
@@ -101,7 +104,7 @@ echo y # clear mbr
 sync
 partprobe /dev/sda
 echo "overwrite sda"
-dd if=/root/chr.img of=/dev/sda bs=4M
+dd if=/run/chr.img of=/dev/sda bs=4M
 sync
 partprobe /dev/sda
 (
